73cfe047dbd91c5fd89edf9c40920ef2b3b27320
KAMILA ROVANI ANGELI <kamila_angeli@estudante.sesisenai.org.br>
2026-03-04 08:54:33 -0300

    PA

--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 import Fastify from 'fastify'
+import { Pool } from 'pg'
+
+
+const sql = new Pool({
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
+})
+
 
 const servidor = Fastify()
 
-servidor.get('/usuarios', () => {
-    return 'Está funcionando!'
+
+servidor.get('/usuarios', async () => {
+    const resultado = await sql.query('select * from usuario')
+    return resultado.rows
+})
+servidor.post ('/usuarios', async (request, reply) => {
+    const nome = request.body.nome;
+    const senha = request.body.senha;
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha)VALUES ($1, $2)', [nome, senha])
+    return 'usuario cadastrado'
+
+})
+
+servidor.put('/usuarios/:id', async (request, reply) => {
+    const body = request.body;
+    const id = request.params.id
+    const resultado = await sql.query('UPDATE usuario SET nome = $1, senha = $2 WHERE id = $3', [body.nome, body.senha, id])
+    return 'Usuário alterado!'
 })
 
 servidor.listen({
-    port:3000
+    port: 3000
 })
